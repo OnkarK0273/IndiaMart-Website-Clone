68c62b25ca2cacfae6c34ffdc726c8fd66949c49
Papri Saha <paprisaha1999@gmail.com>
2023-03-29 21:31:52 +0530

    bulb commit

--- back-end/index.js
+++ back-end/index.js
@@ -1,45 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const userRoute = require('./routes/user.route')
-const plywoodRoute = require('./routes/plywood.product.route')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user.route");
+const plywoodRoute = require("./routes/plywood.product.route");
+require("dotenv").config();
+const app = express();
+const { bulbRouter } = require("./routes/bulb.route");
 
 // global middleweres---------------
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //  routes--------------------------
 
 // route-1
-app.use('/users',userRoute)
+app.use("/users", userRoute);
 
-//route-2 
-app.use('/plywood',plywoodRoute)
+//route-2
+app.use("/plywood", plywoodRoute);
 
-
+//route-3
+app.use("/bulb", bulbRouter);
 
 // run http and back-end server-------------------
 
-app.listen(process.env.HTTP_PORT,async()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGO_CONNECT}`)
-        console.log('connected db')
-
-    }catch(err){
-        console.log('not-connected')
-        console.log(err)
-
-    }
-    console.log('port 4500 running')
-})
-
-
-
-
-
-
-
-
+app.listen(process.env.HTTP_PORT, async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_CONNECT}`);
+    console.log("connected db");
+  } catch (err) {
+    console.log("not-connected");
+    console.log(err);
+  }
+  console.log("port 4500 running");
+});
