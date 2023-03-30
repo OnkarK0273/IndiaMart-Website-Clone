4039fe69a8da8cc64b3732ea7d8c50ff38c9b93a
onkar-kalsannawar <onkarok0273@gmail.com>
2023-03-30 23:00:08 +0530

    build homegrid,posters,footer in home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import Footer from './components/home/Footer';
 import Navbar from './components/home/Nav';
 import AllRoutes from './routes/AllRoutes';
-
+import { useLocation } from "react-router-dom";
 function App() {
+  const location = useLocation()
   return (
-    <div >
-      <Navbar/>
+    <div className='App' >
+      {
+        location.pathname === '/adminDashboard' || location.pathname === '/addProduct' ? false :  <Navbar/>
+      }
+      
       <AllRoutes/>
+
+      {
+        location.pathname === '/adminDashboard' || location.pathname === '/addProduct' ? false :  <Footer/>
+      }
+      
     </div>
   );
 }
--- front-end/src/routes/AllRoutes.jsx
+++ front-end/src/routes/AllRoutes.jsx
@@ -1,13 +1,18 @@
+import { Heading } from '@chakra-ui/react';
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Sign from '../pages/Sign';
 
 export default function AllRoutes() {
   return (
     <>
         <Routes>
-            <Route path={"/"} element={<Home />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
+            <Route path={"/"} element={<Home/>} />
+            <Route path={"/login"} element={<Login/>} />
+            <Route path={"/sign"} element={<Sign/>}/>
+            <Route path="*" element={<Heading h='55vh' >Page not found</Heading>} />
         </Routes>
     </>
   )
