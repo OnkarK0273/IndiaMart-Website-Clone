681a5304bca31c632a45304cac414ddf5148860b
Papri Saha <paprisaha1999@gmail.com>
2023-03-30 23:39:47 +0530

    papri_day-3

--- back-end/routes/bulb.routes.js
+++ back-end/routes/bulb.routes.js
@@ -7,7 +7,7 @@ const auth = require("../middleware/auth.middleware");
 
 // Read
 bulbRouter.get("/", async (req, res) => {
-  const {token} = req.headers;
+  const { token } = req.headers;
   // console.log(token);
   const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
@@ -58,54 +58,5 @@ bulbRouter.delete("/delete/:bulbID", auth, async (req, res) => {
     res.status(400).send({ msg: error.message });
   }
 });
-
-module.exports = { bulbRouter };
-
-// // get
-
-// bulbRouter.get("/", async (req, res) => {
-//   try {
-//     const bulb = await BulbModel.find();
-//     res.status(200).send({ data: bulb });
-//   } catch (err) {
-//     res.status(200).send({ error: err.message });
-//   }
-// });
-
-// // post
-
-// bulbRouter.post("/add", auth, async (req, res) => {
-//   try {
-//     const bulb = new BulbModel(req.body);
-//     const result = await bulb.save();
-//     res.status(200).send({ data: result });
-//   } catch (err) {
-//     res.status(400).send({ err: err.message });
-//   }
-// });
-
-// // patch /update
-
-// bulbRouter.patch("/update/:id", auth, async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     await BulbModel.findByIdAndUpdate({ _id: id }, req.body);
-//     res.status(200).send({ msg: "bulb updated successfully..." });
-//   } catch (err) {
-//     res.status(400).send({ error: err.message });
-//   }
-// });
-
-// // delete
-
-// bulbRouter.delete("/delete/:id", auth, async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     await BulbModel.findByIdAndDelete({ _id: id });
-//     res.status(200).send({ msg: "bulb deleted successfully..." });
-//   } catch (err) {
-//     res.status(400).send({ error: err.message });
-//   }
-// });
 
 module.exports = { bulbRouter };
